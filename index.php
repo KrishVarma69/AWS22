4840c606808f4e6efa4c0e0acc99adc6dd8b3556
KrishVarma69 <52405615+KrishVarma69@users.noreply.github.com>
2022-06-11 13:13:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 $t = date("H");
 
 if ($t < "20") {
-  echo "Hello World! Have an Awesome day! yes awsesome day for the events to trigger";
+  echo "Hello World! Let's have an Awesome day! yes awsesome day for the events to trigger";
 }
 ?>
  
